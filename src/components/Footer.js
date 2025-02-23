fe37ebd756a5c558672f0889ad419f4e1bd690d8
Swapnil Zade <zade.swapnil@gmail.com>
2025-02-23 10:26:56 +0530

    Initial commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,10 +9,11 @@ const Footer = () => {
             <div className="footer-bottom">
                 <div className="footer-container">
                     <p className="copyright">&copy; {new Date().getFullYear()} Asia Tourer. All rights reserved.</p>
-                    <ul className="footer-links">
+                    
+                    {/*<ul className="footer-links">
                         <li><Link to="/privacy">Privacy Policy</Link></li>
                         <li><Link to="/terms">Terms of Service</Link></li>
-                    </ul>
+                    </ul>*/}
                 </div>
             </div>
         </footer>
--- src/components/Header.js
+++ src/components/Header.js
@@ -11,10 +11,8 @@ const Header = () => {
         <header className="header">
             <div className="header-container">
                 <div className="header-left">
-                    <button className="menu-button" onClick={() => setIsMenuOpen(!isMenuOpen)}>
-                        <FaBars />
-                        <span>Menu</span>
-                    </button>
+                    
+                    
                     <Link to="/" className="logo">
                         <img src="https://asiatourer.com/wp-content/uploads/2025/02/Untitled-design-1.png" alt="Asia Tourer Logo" />
                         <div className="logo-text">
@@ -51,17 +49,6 @@ const Header = () => {
                     </div>
                 </div>
             </div>
-            {isMenuOpen && (
-                <nav className="vertical-menu">
-                    <ul>
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/destinations">Destinations</Link></li>
-                        <li><Link to="/tours">Tours</Link></li>
-                        <li><Link to="/about">About Us</Link></li>
-                        <li><Link to="/contact">Contact</Link></li>
-                    </ul>
-                </nav>
-            )}
         </header>
     );
 };
